e7120ce1f08d95edb5a67d8e7ba371129673c8f0
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2021-02-07 18:47:26 +0700

    import message

--- components/HistoryTable/HistoryTable.js
+++ components/HistoryTable/HistoryTable.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Space, Popconfirm, Popover } from "antd";
+import { Space, Popconfirm, Popover, message } from "antd";
 import {
   DeleteOutlined,
   EditOutlined,
