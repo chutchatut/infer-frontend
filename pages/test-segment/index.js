8b86fd1966934d25ab4c466fdd100811fc42b496
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2021-02-17 08:30:53 +0700

    remove test-segment

--- pages/test-segment/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment, useEffect, useState } from "react";
-import SegmentationEditor from "../../components/SegmentationEditor/SegmentationEditor";
-
-const segmentationEditor = (props) => {
-  return <SegmentationEditor />;
-};
-
-export default segmentationEditor;
